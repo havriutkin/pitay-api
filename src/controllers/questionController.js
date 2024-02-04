b806a77b7138fc0741ad4c14ac50c5ed918a30a6
havriutkin <havriutkin@gmail.com>
2024-02-04 15:19:06 -0500

    Add auth to update method

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -34,15 +34,23 @@ module.exports.getByLessonId = async (req, res, next) => {
     }
 }
 
+// Can only update isAnswered state
 module.exports.update = async (req, res, next) => {
     // TODO
-    // ! Auth
     try {
         const questionId = req.params.questionId;
         const currentQuestionObj = (await questionService.getQuestionById({questionId}))[0];
+
+        // Check if user owns this lesson
+        const lesson = await lessonService.getLessonById({lessonId: currentQuestionObj.lesson_id});
+        if (lesson[0].owner_id !== req.user.id) {
+            return res.status(401).json({
+                message: 'Not owner.'
+            });
+        }
         
         const isAnswered = req.body.isAnswered || currentQuestionObj.isAnswered;
-        const question = req.body.question || currentQuestionObj.question;
+        const question = currentQuestionObj.question;
 
         await questionService.updateQuestionById({questionId, question, isAnswered});
         res.status(200).json({
